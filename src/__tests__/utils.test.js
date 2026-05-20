7d0cfb11043bf126ae4f88837541e1925b3258b2
Sharon Moegi <sharamoegi@gmail.com>
2026-05-20 15:46:37 +0300

    Set Up Test Case

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,7 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+describe("pointsForWord", () => {
+    it("calculates the total points for a words(1 point per vowel, 2 per consonant)", () => {
+        //Write Test Code here
+    })
+})
